15fb4806f1c9d358bb81f65ce1a7da7ef64e7a74
Mario <50754789+CaptainDouche@users.noreply.github.com>
2019-05-19 20:56:12 +0200

    Delete lacrosse2mqtt.sh

--- lacrosse2mqtt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# create a named pipe where the mqtt_pub will read from
-MQFIFO=/tmp/mqfifo
-test -p ${MQFIFO} || mkfifo ${MQFIFO}
-
-/bin/rfm69_lacrosse --format "%x/%t=%v %u\n" --output ${MQFIFO} --daemon
-/bin/mqtt_cli --publish "sensors/" --delim "=" --input ${MQFIFO} --daemon
-
